1dc00ccdad10638f7b549fcaf30d56fa68b01a8a
Fernando Scatolin <m2rg@outlook.com>
2021-02-23 18:30:40 -0300

    Fail and Always

--- xhr.base.js
+++ xhr.base.js
@@ -1,27 +1,20 @@
 var XHR = new function () {
-    var notificationAjax = function (res, callback, thrownCatch) {
-        try{
-            if (res) {
-                if (callback){
-                    callback(res);
-                }
-            } else {
-                Swal.fire({
-                    title: "Fail",
-                    html: res ? res : "Call don't return result",
-                    type: 'error',
-                    allowOutsideClick: false
-                });
+    var notificationAjax = function (res, callback) {
+        if (res) {
+            if (callback){
+                callback(res);
             }
-        }
-        catch{
-            if (thrownCatch){
-                thrownCatch();
-            }
+        } else {
+            Swal.fire({
+                title: "Fail",
+                html: res ? res : "Call don't return result",
+                type: 'error',
+                allowOutsideClick: false
+            });
         }
     };
 
-    this.executeAjax = function (target, prms, callback, thrownCatch, always) {
+    this.executeAjax = function (target, prms, callback, failed, always) {
         $.ajax({
             url: target,
             type: 'post',
@@ -31,7 +24,13 @@ var XHR = new function () {
             async: true
         })
             .done(function (res) {
-                notificationAjax(res.d, callback, thrownCatch);
+                notificationAjax(res.d, callback);
+            })
+            .fail(function(res){
+                failed(res);
+            })
+            .always(function(){
+                always();
             });
     };
 }
